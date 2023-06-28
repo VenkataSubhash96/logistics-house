62645283bdccd97c7635a9e58a91f1df60539a0b
VenkataSubhash96 <subhash.bharadwaj@clark.de>
2023-06-28 15:33:41 +0200

    Add specs for bookings channel

--- spec/channels/bookings_channel_spec.rb
+++ spec/channels/bookings_channel_spec.rb
@@ -1,5 +1,19 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe BookingsChannel, type: :channel do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#subscribed' do
+    it 'successfully subscribes to the channel' do
+      subscribe
+
+      expect(subscription).to be_confirmed
+    end
+
+    it 'broadcasts a welcome message to the subscribed user' do
+      subscribe
+
+      expect(subscription).to have_stream_from('bookings_channel')
+    end
+  end
 end
